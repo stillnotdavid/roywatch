1d1b30ca2f0b93fe96e15c1090949836be162f6e
Rob Zaenglein <rob@Robs-MacBook-Air.local>
2015-10-29 01:30:07 -0400

    draw logic and removed background image for now

--- app/scripts/indexController.js
+++ app/scripts/indexController.js
@@ -1,9 +1,19 @@
 angular.module('roywatch', [])
 .controller('indexController', ['$http', function(http) {
     var vm = this;
-    //for local dev
-    vm.stats = {"mcdavid":{"gamesPlayed":{"display":"games played","stat":10},"goals":{"display":"goals","stat":5},"assists":{"display":"assists","stat":5},"points":{"display":"points","stat":10},"shots":{"display":"shots on goal","stat":20},"shootingPercentage":{"display":"shooting percentage","stat":25},"plusMinus":{"display":"plus / minus","stat":-3},"ppg":{"display":"power play goals","stat":2},"shg":{"display":"short handed goals","stat":0},"gwg":{"display":"game winning goals","stat":1}},"eichel":{"gamesPlayed":{"display":"games played","stat":9},"goals":{"display":"goals","stat":3},"assists":{"display":"assists","stat":0},"points":{"display":"points","stat":3},"shots":{"display":"shots on goal","stat":27},"shootingPercentage":{"display":"shooting percentage","stat":11.1111},"plusMinus":{"display":"plus / minus","stat":-6},"ppg":{"display":"power play goals","stat":2},"shg":{"display":"short handed goals","stat":0},"gwg":{"display":"game winning goals","stat":1}}};
-    /*http.get('/stats', function(data) {
-        vm.stats = data;
-    });*/
+    http.get('/stats').then(function(data){
+        vm.stats = data.data;
+        $(".loader").fadeOut("slow");
+        $('.number').each(function () {
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 700,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        });
+    });
 }]);
